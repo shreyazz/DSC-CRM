4c78e45b51f7b8af6b032ee75f7382039ca517aa
Shreyas Pahune <pahuneshreyas@gmail.com>
2023-01-02 17:21:24 +0530

    feat: home page + routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import Navbar from "./components/Navbar/Navbar";
+import BlogsPage from "./pages/BlogsPage/BlogsPage";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 const App = () => {
+  const [tokenPresent, setTokenPresent] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setTokenPresent(true);
+    } else {
+      setTokenPresent(false);
+    }
+  }, []);
+
+  const navigate = useNavigate();
   return (
     <div>
-      <h1 className="text-3xl font-bold underline font-sans">Hello world!</h1>
+    <Navbar/>
+    <Routes>
+        {tokenPresent ? <Route path="/" index element={<HomePage/>}/> :  <Route path="/" index element={<div className="h-[90vh] w-full bg-slate-100 px-16 py-5 flex items-center justify-center"><h1 className="text-red-600 text-4xl"> Not Authorized </h1></div>}/>}
+        {tokenPresent ? <Route path="/blogs"  element={<BlogsPage/>}/> :  <Route path="/" index element={<div className="h-[90vh] w-full bg-slate-100 px-16 py-5 flex items-center justify-center"><h1 className="text-red-600 text-4xl"> Not Authorized </h1></div>}/>}
+        <Route path="/login"  element={<LoginPage/>}/>
+    </Routes>
     </div>
+
   );
 };
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./HomePage.css";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [userDetails, setUserDetails] = useState({});
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const getBlogs = async () => {
+      const blogRes = await axios.get(
+        "https://dsc-api.vercel.app/api/blog/getAll"
+      );
+      setBlogs(blogRes.data.blogs);
+    };
+    getBlogs();
+    const getUserDetails = () => {
+      const token = localStorage.getItem('token');
+      if(token){
+        setUserDetails(jwtDecode(token));
+      }
+    };
+    getUserDetails();
+  }, [])
+ 
+  return (
+    <div className="h-[90vh] w-full bg-slate-100 px-16 py-5 ">
+      <h1 className="text-2xl font-medium mb-5">Hello <span className="capitalize"> {userDetails.name},</span></h1>
+      <div className="flex gap-3 flex-col">
+        <span className="text-zinc-500 text-sm">Email: <span className="text-zinc-400"> {userDetails.email} </span></span>
+        <span className="text-zinc-500 text-sm">Number: <span className="text-zinc-400"> {userDetails.number} </span></span>
+      </div>
+      <h3 className="mt-9 text-2xl">Recent Blogs</h3>
+      <div className="flex flex-wrap w-full gap-7 ">
+            {blogs.length > 0 &&
+              blogs.map((b, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="w-[fit-content] p-4 bg-white border border-gray-200 rounded-lg shadow-md mt-5"
+                  >
+                      <img
+                        src={b.image}
+                        alt=""
+                        className="h-36 w-56 rounded-sm mb-3"
+                      />
+                      <h5 className="text-lg font-bold tracking-tight text-zinc-600">
+                        {b.title}
+                      </h5>
+                    <p className="mb-3 text-sm text-gray-700 dark:text-gray-400">
+                      {b.category}
+                    </p>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                      {b.body.slice(0, 20)}...
+                    </p>
+                    
+                  </div>
+                );
+              })}
+          </div>
+    
+    </div>
+  );
 };
 
 export default HomePage;
